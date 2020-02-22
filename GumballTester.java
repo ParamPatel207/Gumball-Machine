f3e25f994b7cb9a62c0bd3f67b7d1aadb4b43365
ParamPatel207 <35900835+ParamPatel207@users.noreply.github.com>
2020-02-22 14:21:31 -0800

    Update GumballTester.java

--- GumballTester.java
+++ GumballTester.java
@@ -3,20 +3,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class GumballTester {
-
+	//testing all correct inputs
 	@Test
 	void test() {
 		GumballMachine test1 = new GumballMachine();
 		//insert coins
 		test1.insertCoin(0.05);
-		test1.insertCoin(0.1);
-		//invalid coin hence returned all coin
-		test1.insertCoin(0.3);
-		assertEquals(test1.moneyleft(),0.0);
-		//insert coins
-		test1.insertCoin(0.05);
 		test1.insertCoin(0.25);
-		//get red gum ball
 		test1.getRedGumBall();
 		//total money left as red gumball cost 0.05 is 0.25
 		assertEquals(test1.moneyleft(),0.25);
@@ -24,5 +17,87 @@ class GumballTester {
 		//nothing left
 		assertEquals(test1.moneyleft(),0.0);
 	}
-
+	//wrong coin insert
+	@Test
+	void test1() {
+		GumballMachine test2 = new GumballMachine();
+		test2.insertCoin(0.05);
+		//invalid coin hence returned all coin
+		test2.insertCoin(0.3);
+		assertEquals(test2.moneyleft(),0.0);
+		//insert coins
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.25);
+		//get red gum ball
+		test2.getRedGumBall();
+		assertEquals(test2.moneyleft(),0.25);
+		assertEquals(test2.returnChange(),0.25);
+		assertEquals(test2.moneyleft(),0.0);
+	}
+	//negative coins and 2 different ball and also negative moneyleft check
+	@Test
+	void test2()
+	{
+		GumballMachine test2 = new GumballMachine();
+		test2.insertCoin(-1);
+		//invalid coin hence returned all coin
+		test2.insertCoin(0.3);
+		assertEquals(test2.moneyleft(),0.0);
+		System.out.println(test2.moneyleft());
+		//insert coins
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		//get red gum ball
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		assertEquals(0.0,test2.returnChange());
+		assertEquals(0.0,test2.moneyleft());
+		
+	}
+	//not enough money
+	@Test
+	void test3()
+	{
+		GumballMachine test2 = new GumballMachine();
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		//get red gum ball
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		//not enough money in print
+		//not enough money in print
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		assertEquals(0.0,test2.returnChange());
+		assertEquals(0.0,test2.moneyleft());
+		
+	}
+	//extra coins
+	@Test
+	void test4()
+	{
+		GumballMachine test2 = new GumballMachine();
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.25);
+		test2.insertCoin(0.05);
+		test2.insertCoin(0.10);
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		test2.getRedGumBall();
+		test2.getYellowGumBall();
+		assertEquals(0.45,test2.returnChange());
+		assertEquals(0.0,test2.moneyleft());	
+	}
 }
